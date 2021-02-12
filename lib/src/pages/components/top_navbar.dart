423110b20fdcf98e2613495a958c4ee252cf4fca
Uriel Aispuro <urielalexis64@gmail.com>
2021-02-11 22:34:15 -0700

    top padding

--- lib/src/pages/components/top_navbar.dart
+++ lib/src/pages/components/top_navbar.dart
@@ -3,26 +3,29 @@ import 'package:flutter/material.dart';
 class TopNavbar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        Image(
-          image: AssetImage('assets/img/netflix_logo.png'),
-          width: 30,
-        ),
-        Text(
-          'Series',
-          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-        ),
-        Text(
-          'Movies',
-          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-        ),
-        Text(
-          'My list',
-          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-        ),
-      ],
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Image(
+            image: AssetImage('assets/img/netflix_logo.png'),
+            width: 30,
+          ),
+          Text(
+            'Series',
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            'Movies',
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            'My list',
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
     );
   }
 }
