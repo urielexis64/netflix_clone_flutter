1984c0f8cd350260427c4df76b7cfd7eb8d542d9
Uriel Aispuro <urielalexis64@gmail.com>
2021-02-11 22:24:58 -0700

    body, mainposter, horizontalList

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:netflix_clone/src/pages/components/main_poster.dart';
+import 'package:netflix_clone/src/pages/components/rounded_item.dart';
+import 'package:netflix_clone/src/pages/components/square_item.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -11,8 +13,47 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _createBody() {
+    return ListView(
+      children: [
+        MainPoster(),
+        _horizontalList(
+            'Avances',
+            RoundedItem(
+              imageURL:
+                  'https://3.bp.blogspot.com/-qvw0-RjIjyc/W5gPSBHfSeI/AAAAAAAAh6E/QBCS4YQVKlU7Ur1p0dCfG1WM2N8EB0OcQCLcBGAs/s640/Elite_Official_Poster_JPosters.jpg',
+              bottomImageURL:
+                  'https://www.pikpng.com/pngl/b/255-2550276_elite-elite-serie-logo-png-clipart.png',
+              borderColor: Colors.redAccent,
+            ),
+            10),
+        _horizontalList('Películas de miedo', SquareItem(), 20),
+        _horizontalList('Películas de miedo', SquareItem(), 20),
+        _horizontalList('Películas de miedo', SquareItem(), 20),
+      ],
+    );
+  }
+
+  Widget _horizontalList(String title, Widget widget, int items) {
     return Column(
-      children: [MainPoster()],
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 10),
+          child: Text(
+            title,
+            style: TextStyle(
+                fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+        ),
+        Container(
+          height: 110,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (context, index) => widget,
+            itemCount: items,
+          ),
+        ),
+      ],
     );
   }
 }
