61c572486553b63e1c92b430d1f3d0eec00c3556
Uriel Aispuro <urielalexis64@gmail.com>
2021-02-11 21:23:27 -0700

    title and home

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:netflix_clone/src/pages/home_page.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
-    );
+    return MaterialApp(title: 'Netflox App', home: HomePage());
   }
 }
