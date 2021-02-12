b54c27a499fdbe0a70ff28fbc5df2621e7db6d53
Uriel Aispuro <urielalexis64@gmail.com>
2021-02-11 23:09:17 -0700

    Bottom Navigation style changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:netflix_clone/src/pages/home_page.dart';
 
 void main() => runApp(MyApp());
@@ -6,6 +7,10 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light.copyWith(
+        statusBarColor: Colors.transparent,
+        systemNavigationBarDividerColor: Colors.red));
+
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Netflox App',
