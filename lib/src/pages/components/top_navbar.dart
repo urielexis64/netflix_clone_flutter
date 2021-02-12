84e6b2c7363041f274884cbbca540587edc2e326
Uriel Aispuro <urielalexis64@gmail.com>
2021-02-11 21:24:00 -0700

    top navbar widget

---
+++ lib/src/pages/components/top_navbar.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class TopNavbar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Image(
+          image: AssetImage('assets/img/netflix_logo.png'),
+          width: 30,
+        ),
+        Text(
+          'Series',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        Text(
+          'Movies',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        Text(
+          'My list',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+      ],
+    );
+  }
+}
